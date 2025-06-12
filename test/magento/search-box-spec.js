c6ac6a96f0e4d18db92455b10f1cbe5a8ed2a1ff
Testtsts <ihsannur0610@gmail.com>
2025-06-12 12:13:18 +0700

    search box spec

---
+++ test/magento/search-box-spec.js
@@ -0,0 +1,14 @@
+const {HomePage,HOME_URL,} = require('./home-page')
+const { browser, expect } = require('@wdio/globals')
+const {CatalogSearch} = require("./catalog-search-page")
+
+describe("magento search function", ()=>{
+    it("using jacket as keyword should return relevant products",async()=>{
+        await HomePage.visitPage()
+        expect(browser).toHaveUrl(HOME_URL);
+        await HomePage.searchForItem("jacket");
+        expect(browser).toHaveUrl(/CatalogSearch/)
+        await expect(await CatalogSearch.getNthProduct(0)).toHaveText(/Jacket/)
+    })
+
+})
